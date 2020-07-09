d880ab5e7156bb19ee029efbf2f505b418c47139
Wincenty Bertoni Lech <Wblech@MacBook-Pro-de-Wincenty.local>
2020-07-09 16:05:11 -0300

    Fixed day01/ex02

--- day01/ex02/pytest.py
+++ day01/ex02/pytest.py
@@ -1,14 +1,36 @@
 from vector import Vector
 
-teste = Vector((10, 15))
-# teste = Vector((10,20,70))
-# print(teste)
-# teste.size = 5
-# print(teste.size)
-# print(teste.array)
-# print(teste)
-sum = []
-sum = teste / 5
-# print(teste)
 
-print(sum)
+vetor = Vector([2.0, 3.0, 6.0])
+vetor2 = Vector([3.0, 4.0, 5.0])
+print(vetor)
+print(vetor.size)
+print("Vetor2", vetor2)
+print("")
+print("SUM:")
+sum = vetor + 5
+print("vetor + 5 = ", sum)
+sum = vetor + vetor2
+print("vetor + vetor2", sum)
+print("")
+print("SUB:")
+sum = 5 - vetor
+print("5 - vetor", sum)
+sub = vetor - 5
+print("vetor - 5 = ", sub)
+sub = vetor - vetor2
+print("vetor - vetor2", sub)
+print("")
+print("DIV:")
+sum = 5 / vetor
+print("5 / vetor", sum)
+sub = vetor / 5
+print("vetor / 5 = ", sub)
+print("")
+print("MUL:")
+mul = vetor * vetor2
+print("vetor * vetor2",mul)
+mul = vetor * 5
+print("vetor * 5", mul)
+mul = 5 * vetor
+print("5 * vetor", mul)
--- day01/ex02/vector.py
+++ day01/ex02/vector.py
@@ -1,95 +1,194 @@
 class Vector:
-    def __init__(self, array):
-        self.array = array
-        self.__size()
+
+    def __init__(self, values):
+        self.values = values
+
+    @staticmethod
+    def itol(value):
+        i = 0
+        ret = []
+        while i < value:
+            ret.append(float(i))
+            i += 1
+        return ret
+
+    @staticmethod
+    def ttol(value):
+        ret = []
+        for i in range(value[0], value[1]):
+            ret.append(float(i))
+        return ret
+
+    @staticmethod
+    def is_float_list(value):
+        for nbr in value:
+            if not(isinstance(nbr, float)):
+                return False
+        return True
 
     @property
-    def array(self):
-        return self._array
-
-    @array.setter
-    def array(self, value):
-        self._array = []
-        try:
-            if type(value) == int and len(value) == 1:
-                for n in range(value):
-                    self._array.append(float(n))
-            elif type(value) == tuple and len(value) == 2:
-                for n in range(value[0], value[1]):
-                    self._array.append(float(n))
-            elif type(value) == list:
-                for i, n in enumerate(value):
-                    if type(n) != float and type(n) != int:
-                        raise TypeError
-                    elif type(n) is int:
-                        value[i] = float(n)
-                self._array = value
-            else:
-                raise ValueError
-        except TypeError:
-            print("Vector only accepts a floats or int")
-        except ValueError:
-            print(
-                "To make an object use one of this options:\n.Vector()a list \
-of floats: Vector([0.0, 1.0, 2.0, 3.0])\n.A size Vector(3) -> the vector will \
-have values = [0.0, 1.0, 2.0]\n.A range or Vector((10,15)) -> the vector will \
-have values = [10.0, 11.0, 12.0, 13.0, 14.0]")
+    def values(self):
+        return self._values
 
     @property
     def size(self):
-        return self._size
+        return len(self._values)
 
-    def __size(self):
-        self._size = len(self.array)
-
-    def __add__(self, other):
-        for i, n in enumerate(self.array):
-            self._array[i] = n + other
-        return self.array
-
-    def __radd__(self, other):
-        for i, n in enumerate(self.array):
-            self._array[i] = n + other
-        return self.array
-
-    def __sub__(self, other):
-        for i, n in enumerate(self.array):
-            self._array[i] = n - other
-        return self.array
-
-    def __rsub__(self, other):
-        for i, n in enumerate(self.array):
-            self._array[i] = n - other
-        return self.array
-
-    def __truediv__(self, other):
+    @values.setter
+    def values(self, value):
         try:
-            for i, n in enumerate(self.array):
-                self._array[i] = n / other
-        except ZeroDivisionError as e:
-            print(e)
-        else:
-            return self.array
-
-    def __rtruediv__(self, other):
-        try:
-            for i, n in enumerate(self.array):
-                self._array[i] = n / other
-        except ZeroDivisionError as e:
-            print(e)
-        else:
-            return self.array
-
-    def __mul__(self, other):
-        for i, n in enumerate(self.array):
-            self._array[i] = n * other
-        return self.array
-
-    def __rmul__(self, other):
-        for i, n in enumerate(self.array):
-            self._array[i] = n * other
-        return self.array
+            if not value:
+                raise ValueError
+            elif isinstance(value, int):
+                self._values = self.itol(value)
+            elif isinstance(value, tuple):
+                self._values = self.ttol(value)
+            elif isinstance(value, list):
+                if not(self.is_float_list(value)):
+                    raise TypeError
+                self._values = value
+            else:
+                raise TypeError
+        except ValueError:
+            print("Error:\nNo argument found")
+        except TypeError:
+            print("Error:\n\
+You should be able to initialize the object with:\n\
+• a list of floats: Vector([0.0, 1.0, 2.0, 3.0])\n\
+• a size Vector(3) -> the vector will have values = [0.0, 1.0, 2.0]\n\
+• a range or Vector((10,15)) -> the vector will have values = [10.0, 11.0, \
+12.0, 13.0, 14.0]\
+                ")
 
     def __str__(self):
-        msg = f"{self.array}"
-        return msg
+        txt = f"Vector {self.values}"
+        return txt
+
+    def __add__(self, other):
+        ret = []
+        try:
+            if not(isinstance(other, (Vector, int, float))):
+                raise TypeError
+            elif isinstance(other, Vector):
+                if self.size != other.size:
+                    raise ValueError
+                for i in range(0, self.size):
+                    ret.append(self.values[i] + other.values[i])
+            else:
+                for t in range(0, self.size):
+                    ret.append(self.values[t] + other)
+        except TypeError:
+            print("Error:\nVector instance can only be sum with another \
+Vector instance or with a scalar(int, float)")
+        except ValueError:
+            print("Error:\nVector instance can only be sum with the same \
+dimension")
+        return ret
+
+    def __radd__(self, other):
+        ret = []
+        try:
+            if not(isinstance(other, (int, float))):
+                raise TypeError
+            else:
+                for t in range(0, self.size):
+                    ret.append(self.values[t] + other)
+        except TypeError:
+            print("Error:\nVector instance can only be sum with another \
+Vector instance or with a scalar(int, float)")
+        return ret
+
+    def __sub__(self, other):
+        ret = []
+        try:
+            if not(isinstance(other, (Vector, int, float))):
+                raise TypeError
+            elif isinstance(other, Vector):
+                if self.size != other.size:
+                    raise ValueError
+                for i in range(0, self.size):
+                    ret.append(self.values[i] - other.values[i])
+            else:
+                for t in range(0, self.size):
+                    ret.append(self.values[t] - other)
+        except TypeError:
+            print("Error:\nVector instance can only be sub with another \
+Vector instance or with a scalar(int, float)")
+        except ValueError:
+            print("Error:\nVector instance can only be sub with the same \
+dimension")
+        return ret
+
+    def __rsub__(self, other):
+        ret = []
+        try:
+            if not(isinstance(other, (int, float))):
+                raise TypeError
+            else:
+                for t in range(0, self.size):
+                    ret.append(self.values[t] - other)
+        except TypeError:
+            print("Error:\nVector instance can only be sub with another \
+Vector instance or with a scalar(int, float)")
+        return ret
+
+    def __truediv__(self, other):
+        ret = []
+        try:
+            if not(isinstance(other, (int, float))):
+                raise TypeError
+            else:
+                for t in range(0, self.size):
+                    ret.append(self.values[t] / other)
+        except TypeError:
+            print("Error:\nVector instance can only be div with a \
+scalar(int, float)")
+        return ret
+
+    def __rtruediv__(self, other):
+        ret = []
+        try:
+            if not(isinstance(other, (int, float))):
+                raise TypeError
+            else:
+                for t in range(0, self.size):
+                    ret.append(self.values[t] / other)
+        except TypeError:
+            print("Error:\nVector instance can only be div with a \
+scalar(int, float)")
+        return ret
+
+    def __mul__(self, other):
+        try:
+            if not(isinstance(other, (Vector, int, float))):
+                raise TypeError
+            elif isinstance(other, Vector):
+                ret = 0
+                if self.size != other.size:
+                    raise ValueError
+                for i in range(0, self.size):
+                    ret += self.values[i] * other.values[i]
+            else:
+                ret = []
+                for t in range(0, self.size):
+                    ret.append(self.values[t] * other)
+        except TypeError:
+            print("Error:\nVector instance can only be multiplication \
+with another Vector instance or with a scalar(int, float)")
+        except ValueError:
+            print("Error:\nVector instance can only be multiplicated with \
+the same dimension")
+        return ret
+
+    def __rmul__(self, other):
+        ret = []
+        try:
+            if not(isinstance(other, (int, float))):
+                raise TypeError
+            else:
+                for t in range(0, self.size):
+                    ret.append(self.values[t] * other)
+        except TypeError:
+            print("Error:\nVector instance can only be multiplicated with \
+another Vector instance or with a scalar(int, float)")
+        return ret
